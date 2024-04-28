af2d842cfddf122adec706cd5281e1d7e2e614d4
Samuel Fuentes (Genius) <samuelgenius1034@gmail.com>
2024-04-27 21:38:36 -0400

    First commit

---
+++ blog/models.py
@@ -0,0 +1,12 @@
+import django.contrib.auth.models
+from ckeditor.fields import RichTextField
+from ckeditor_uploader.fields import RichTextUploadingField
+from django.db import models
+
+# Create your models here.
+class Post (models.Model):
+    title = models.CharField(max_length=255)
+    summary = RichTextField()
+    content = RichTextUploadingField()
+    author = models.ForeignKey(django.contrib.auth.models.User, on_delete=models.PROTECT)
+    created_at = models.DateField(auto_now_add=True)
